0a3e39daa24556f2e12b9539087cf638e0e5e587
fhomps <hompsfrancois@gmail.com>
2021-02-12 06:16:54 +0000

    Added live backup functionality

--- backup.sh
+++ backup.sh
@@ -1,26 +1,99 @@
 #!/bin/bash
 
 NUMBACKUPS=20
+WORLDFILES=( "world" "world_nether" "world_the_end" )
 
-if screen -list | grep -q "minecraft" ; then
-  echo "Server still running!"
+startmsg='Starting live backup, the server may lag for a bit.'
+donemsg='Live backup done!'
+
+for arg in "$@" ; do
+  case $arg in
+    --live)
+    live=true
+    shift
+    ;;
+    --noskip)
+    noskip=true
+    shift
+    ;;
+    *)
+    echo "Unrecognized argument $arg"
+    exit 1
+    ;;
+  esac
+done
+
+loghead='^\[..:..:..] \[Server thread\/INFO\]: '
+
+if [ "$live" = true ] ; then
+  echo "Attempting live backup..."
+  if ! screen -list | grep -q "minecraft" ; then
+    echo "Server not started!"
+    exit 1
+  fi
 else
-  cd ~/paper
-  if grep -q "joined the game" logs/latest.log ; then
-    echo "Backing up world..."
-    BACKUPTIME=`date +%d-%m-%y_%H:%M:%S`
-    DESTINATION=backup/$BACKUPTIME.tar.gz
-    tar -cpzf $DESTINATION world world_nether world_the_end
-    echo "Backup done."
-    cd backup
-    if [ `ls | wc -l` -gt $NUMBACKUPS ] ; then
-      echo "Too many backups, purging..."
-      while [ `ls | wc -l` -gt $NUMBACKUPS ] ; do
-        rm "$(ls -t | tail -1)"
-      done
-    fi
-    cd ..
-  else
-    echo "No player logged in since last restart, skipping backup."
+  echo "Attempting offline backup..."
+  if screen -list | grep -q "minecraft" ; then
+    echo "Server still running!"
+    exit 1
   fi
 fi
+
+cd ~/paper
+
+if ! [ "$noskip" = true ] \
+   && ! tac logs/latest.log | sed "/${loghead}\[Server\] ${startmsg}/q" | tac | \
+          grep -q "${loghead}There are [1-9][0-9]* of a max of [0-9]\+ players online" \
+   && ! tac logs/latest.log | sed "/${loghead}\[Server\] ${donemsg}/q" | tac | \
+          grep -q "${loghead}\w* joined the game" ; then
+  echo "No players online since last backup or restart, skipping backup"
+  exit 0
+fi
+
+if [ "$live" = true ] ; then
+  echo "Announcing backup..."
+  screen -S minecraft -X stuff "say ${startmsg}^M"
+  screen -S minecraft -X stuff "save-off^M"
+  screen -S minecraft -X stuff "save-all flush^M"
+  # List current players to let next backups to know if there was player activity between backups
+  screen -S minecraft -X stuff "list^M"
+  # Wait for the save-all command to start processing before testing to see if it finished
+  # (the write to latest.log is not instant)
+  echo "Waiting for world flush..."
+  sleep 1
+  # Wait for the save-all command to stop processing
+  while ! tac logs/latest.log | sed "/${loghead}Saving the game/q" | tac | \
+            grep -q "${loghead}Saved the game" ; do
+    sleep 0.1
+  done
+  echo "World flush done, backing up..."
+fi
+
+echo "Backing up world..."
+BACKUPTIME=`date +%d-%m-%y_%H:%M:%S`
+COPYDIR=backup/${BACKUPTIME}_temp
+mkdir $COPYDIR
+cp -r ${WORLDFILES[@]} $COPYDIR
+echo "Copied world files to ${COPYDIR}, compressing..."
+
+if [ "$live" = true ] ; then
+  screen -S minecraft -X stuff "save-on^M"
+fi
+
+tar -cpzf backup/${BACKUPTIME}.tar.gz ${COPYDIR}/*
+rm -rf $COPYDIR
+
+if [ "$live" = true ] ; then
+  screen -S minecraft -X stuff "say ${donemsg}^M"
+fi
+
+echo "Backup done."
+
+cd backup
+if [ `ls | wc -l` -gt $NUMBACKUPS ] ; then
+  echo "Too many backups, purging..."
+  while [ `ls | wc -l` -gt $NUMBACKUPS ] ; do
+    rm "$(ls -t | tail -1)"
+  done
+fi
+cd ..
